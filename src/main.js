480ad4bad9882b6525c7dc049509a266a3d21cdf
Giuseppe Tropea <giuseppe.tropea@netsenseweb.com>
2019-05-27 19:26:26 +0200

    after automatic addition of apollo via "vue add apollo"

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
+import { createProvider } from './vue-apollo'
 
 Vue.prototype.$http = axios;
 
 new Vue({
+  apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount("#app");
